60c39d99b856fc44ab322a7dc27078bab9dfad6c
Constanza Navarrine <cnavarrine@itba.edu.ar>
2025-10-30 14:17:09 -0300

    cambios

---
+++ Pizarrones/04_v2.listaEnlazada.py
@@ -0,0 +1,58 @@
+from typing import Optional
+class Nodo():
+    def __init__(self, dato):
+        self.dato = dato               # dato guardado en el nodo
+        self.siguiente=None
+    def __str__(self):
+        siguiente= self.siguiente.dato if self.siguiente else None
+        return f"{self.dato} y mi siguiente es {siguiente}"
+  
+
+
+class ListaEnlazada:
+    def __init__(self):
+        self.cabeza: Optional[Nodo] = None
+
+    def esta_vacia(self) -> bool:
+        return self.cabeza is None
+
+    def agregar_al_inicio(self, nodo: Nodo) -> None:
+        if not isinstance(nodo, Nodo):
+            raise TypeError("El parámetro recibido no es un Nodo")
+
+        if self.esta_vacia():
+            self.cabeza = nodo
+            return
+
+        nodo.siguiente = self.cabeza
+        self.cabeza = nodo
+
+    def agregar_al_final(self, nodo: Nodo) -> None:
+        if not isinstance(nodo, Nodo):
+            raise TypeError("El parámetro recibido no es un Nodo")
+
+        if self.esta_vacia():
+            self.cabeza = nodo
+            return
+
+        actual = self.cabeza
+        while actual.siguiente:
+            actual = actual.siguiente
+        actual.siguiente = nodo
+
+    def mostrar_lista(self) -> None:
+        if self.esta_vacia():
+            print("La lista está vacía")
+            return
+
+        print("Contenido de la lista enlazada:")
+        actual = self.cabeza
+        valores = []
+
+        # Recorrer la lista hasta que el último sea None
+        while actual:
+            valores.append(str(actual.dato))
+            actual = actual.siguiente
+
+        # Mostrar los valores
+        print(" -> ".join(valores))
